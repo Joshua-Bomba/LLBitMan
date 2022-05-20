8ebb0b2cfeee05e0a1241d209c9b30000dfef046
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-20 00:18:52 -0400

    ISession SetObject & GetObject

--- LLBitMan/LLBitManSessionExtensions.cs
+++ LLBitMan/LLBitManSessionExtensions.cs
@@ -4,10 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace LLBitMan
 {
     public static class LLBitManSessionExtensions
     {
+        public static void SetObject<TProp>(this ISession session, string key, TProp value)
+        {
+            if(value == null)
+            {
+                session.Set(key, null);
+                return;
+            }
+            
+            if (LLDataMan.LLBitMan.TrySetPrimativeValue(value, out byte[] d))
+            {
+                session.Set(key, d);
+                return;
+            }
+            string cereal = JsonConvert.SerializeObject(value);
+            byte[] byteData = Encoding.UTF8.GetBytes(cereal);
+            session.Set(key, byteData);
+        }
+
+        public static TResult GetObject<TResult>(this ISession session, string key)
+        {
+            if (session.TryGetValue(key, out byte[] t))
+            {
+                if (t != null)
+                {
+                    if (LLDataMan.LLBitMan.TryGetPrimitiveValue<TResult>(t, out TResult r))
+                    {
+                        return r;
+                    }
+                }
+            }
+            return default(TResult);
+        }
+
+        public static bool TryGetObject<TResult>(this ISession session, string key, out TResult result)
+        {
+            if (session.TryGetValue(key, out byte[] t))
+            {
+                if(t != null)
+                {
+                    if (LLDataMan.LLBitMan.TryGetPrimitiveValue<TResult>(t, out TResult r))
+                    {
+                        result = r;
+                        return true;
+                    }
+                }               
+            }
+            result = default(TResult);
+            return false;
+        }
     }
 }
